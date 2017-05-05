4102f40574227014fd2d7957722bbc59f926ae3d
xxdetourxx <xxdetourxx@hotmail.com>
2017-05-05 12:02:00 -0400

    Updated UI and JS
    Commiting again, to make sure all files were uploaded.

--- Scripts/parts.js
+++ Scripts/parts.js
@@ -1,3 +1,85 @@
+function getData(gameIndex)
+{
+	var xmlDoc=loadXMLDoc("alpha.xml");
+	console.log("getData gameIndex is:");
+	console.log(gameIndex);
+	var image=xmlDoc.getElementsByTagName("image")[gameIndex].childNodes[0].nodeValue;
+	// this does not work - 
+	document.getElementById("gameImage").innerHTML='<a href="images/catacomb_abyss.jpg"><div id="imagethumb" style="background-image:url(images/thumbs/catacomb_abyss.jpg); background-position:center center; background-repeat:no-repeat;"></div></a>';
+	var name=xmlDoc.getElementsByTagName("name")[gameIndex].childNodes[0].nodeValue;
+	document.getElementById("name").innerText=name;
+	var released=xmlDoc.getElementsByTagName("released")[gameIndex].childNodes[0].nodeValue;
+	document.getElementById("released").innerText=released;
+	var developer=xmlDoc.getElementsByTagName("developer")[gameIndex].childNodes[0].nodeValue;
+	document.getElementById("developer").innerText=developer;
+	var description=xmlDoc.getElementsByTagName("description")[gameIndex].childNodes[0].nodeValue;
+	document.getElementById("description").innerText=description;
+	
+	//debug
+	console.log("image: ");
+	console.log(image);
+	console.log("name: ");
+	console.log(name);
+	console.log("released: ");
+	console.log(released);
+	console.log("developer: ");
+	console.log(developer);
+	console.log("description: ");
+	console.log(description);
+}
+
+function nextClick()
+{
+	var xmlDoc=loadXMLDoc("alpha.xml");
+	var count = xmlDoc.getElementsByTagName('name').length;
+	console.log("Total gameIndex count is: ");
+	console.log(count);
+	
+	if (gameIndex < count){
+		gameIndex = ++gameIndex;
+		console.log("Added 1 to gameIndex, new gameIndex:");
+		console.log(gameIndex);
+		getData(gameIndex);
+	} else if (gameIndex == count){
+		gameIndex = ++gameIndex;
+		if (gameIndex > count){
+			console.log("The end is the beginning.");
+			gameIndex = 0;
+			getData(gameIndex);
+		}
+	}
+}
+
+function prevClick()	
+{
+	var xmlDoc=loadXMLDoc("alpha.xml");
+	var count = xmlDoc.getElementsByTagName('name').length;
+	console.log("Total gameIndex count is: ");
+	console.log(count);
+	
+	if (gameIndex > 0){
+		gameIndex = --gameIndex;
+		console.log("Removed 1 from gameIndex, new gameIndex:");
+		console.log(gameIndex);
+		getData(gameIndex);
+	} else if (gameIndex == 0){
+		gameIndex = --gameIndex;
+		if (gameIndex < 0){
+			console.log("The beginning is the end.");
+			gameIndex = count;
+			getData(gameIndex);
+		}
+	}
+}
+
+function alphaLoad()
+{
+gameIndex = 0;
+console.log("Starting gameIndex should be 0, it is:");
+console.log(gameIndex);
+getData(gameIndex);
+}
+
 function loadXMLDoc(XMLname){
 	var xmlDoc;
 	if (window.XMLHttpRequest){
@@ -15,8 +97,9 @@ function loadXMLDoc(XMLname){
 		return null;
 }
 
+
 function ind_header_html(){
-document.write("<div id='header'><div class='title_text'>.gg/ the_game_gallery_{}</div><div class='topnav'></div></div><hr />");
+document.write("<div id='header'><div class='title_text'>Game Gallery</div><div class='topnav'></div></div><hr />");
 }
 
 function footer_html(){
@@ -24,32 +107,9 @@ document.write("<div id='footer'><hr /><div class='link_text'><a href='home.html
 }
 
 function chron_header_html(){
-document.write("<div id='header'><div class='title_text'>.gg/ the_game_gallery_{}</div><div class='topnav'><a href='alpha.html'>alpha</a>&nbsp;|&nbsp;<a href='chron.html'><span style='color: red;'>chron</span></a></div></div><hr />");
+document.write("<div id='header'><div class='title_text'>Game Gallery </div><div class='topnav'><a href='alpha.html'>alpha</a>&nbsp;|&nbsp;<a href='chron.html'><span style='color: red;'>chron</span></a></div></div><hr />");
 }
 
 function alpha_header_html(){
-document.write("<div id='header'><div class='title_text'>.gg/ the_game_gallery_{<script>document.write(name);</script>}</div><div class='topnav'><a href='alpha.html'><span style='color: red;'>alpha</span></a>&nbsp;|&nbsp;<a href='chron.html'>chron</a></div></div><hr />")
-}
-
-function MM_swapImgRestore() { //v3.0
-  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
-}
-
-function MM_preloadImages() { //v3.0
-  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
-    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
-    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
-}
-
-function MM_findObj(n, d) { //v4.01
-  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
-    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
-  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
-  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
-  if(!x && d.getElementById) x=d.getElementById(n); return x;
-}
-
-function MM_swapImage() { //v3.0
-  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
-   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+document.write("<div id='header'><div class='title_text'>Game Gallery</div><div class='topnav'><a href='alpha.html'><span style='color: red;'>alpha</span></a>&nbsp;|&nbsp;<a href='chron.html'>chron</a></div></div><hr />")
 }
